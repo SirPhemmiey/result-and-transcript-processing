467da0d58e96bb14644efd14fb3b991d6e413cfa
sirphemmiey <oluwafemiakinde@gmail.com>
2018-07-26 10:54:23 -0800

    Add Department

--- addDept.php
+++ addDept.php
@@ -1,35 +1,15 @@
 <?php
-require_once 'library/config.php';
-require_once 'library/functions.php';
+// require_once 'library/config.php';
+// require_once 'library/functions.php';
 
 
 
 ?>
-<html> 
-<head>
-<title>Result  Management System- Login</title>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
-<link href="include/admin.css" rel="stylesheet" type="text/css">
-
-<link href="<?php echo WEB_ROOT;?>include/style.css" rel="stylesheet" type="text/css">
-<link href="<?php echo WEB_ROOT;?>include/main.css" rel="stylesheet" type="text/css">
-
-</head>
-<body>
-<br/>
-<br/>
-<table width="900" border="0" align="center" cellpadding="0" cellspacing="1" class="graybox">
- <tr> 
-  <td><img src="images/complains.jpg" width="900" height="120"></td>
- </tr>
- <tr> 
-  <td valign="top"> <table width="100%" border="0" cellspacing="0" cellpadding="20">
     <tr> 
      <td class="contentArea"> <form method="post" action="process.php?action=addDept" name="frmLogin" id="frmLogin">
-       <p>&nbsp;<a href="index.php"> Click to Go back </a></p>
        <table width="350" border="0" align="center" cellpadding="5" cellspacing="1" bgcolor="#336699" class="entryTable">
         <tr id="entryTableHeader"> 
-         <td>:: Fill in your Details::</td>
+         <td>:: Add Department::</td>
         </tr>
         <tr> 
          <td class="contentArea"> 
@@ -50,10 +30,7 @@ require_once 'library/functions.php';
              <td colspan="2">&nbsp;</td>
              <td>&nbsp;</td>
            </tr>
-           <tr>
-             <td colspan="2">&nbsp;</td>
-             <td>&nbsp;</td>
-           </tr>
+       
            
             <td colspan="2">&nbsp;</td>
             <td><input name="submit" type="submit" id="btnLogin" value="Submit" style="font-size:14px;color:#0066FF;padding:5px 8px;"></td>
@@ -66,7 +43,3 @@ require_once 'library/functions.php';
     </tr>
    </table></td>
  </tr>
-</table>
-<p>Powered By: <a href="http://www.Springlight.org/">Springlight Technology</a></p>
-</body>
-</html>
